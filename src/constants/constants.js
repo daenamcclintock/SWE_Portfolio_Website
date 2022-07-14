288ead0ed6eb68ca91bfb2f0b9f871bea392f1d5
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-14 10:17:19 -0400

    update to projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -52,7 +52,7 @@ export const projects = [
     description: "Luxury real estate application designed for realtors to list properties and users to browse and inquire about properties. LuxEstate features two different user types, full CRUD functionality, and complex logic for searching and filtering properties by location as well as a multitude of metrics. Utilized the MapBox API for visual mapping.",
     image: '/images/LuxEstate.png',
     tags: ['Mongo', 'Express.js', 'React', 'Node'],
-    source: 'https://google.com',
+    source: '',
     backendCode: 'https://github.com/daenamcclintock/Project-4-API',
     frontendCode: 'https://github.com/daenamcclintock/Project-4-Client',
     id: 5,
