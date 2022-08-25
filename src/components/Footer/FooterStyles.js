06f967a0d54cf93fd459ff1c87edf4561679660c
daenamcclintock <daena.mcclintock@gmail.com>
2022-08-25 11:08:02 -0400

    edits to resume

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -2,10 +2,10 @@ import styled from "styled-components"
 
 export const FooterWrapper = styled.section`
 	width: calc(100vw - 96px);
-  max-width: 1040px;
-  padding: 2rem 48px 40px;
-  margin: 1rem auto;
-  box-sizing: content-box;
+	max-width: 1040px;
+	padding: 2rem 48px 40px;
+	margin: 1rem auto;
+	box-sizing: content-box;
 
 
   @media ${props => props.theme.breakpoints.sm} {
