0b539a2bebe217d2e8ac52ec7a26162e559e5754
daenamcclintock <daena.mcclintock@gmail.com>
2022-08-26 12:32:26 -0400

    added footer to the rticles page

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,7 +1,16 @@
 import React, {useState} from 'react'
+import { ReactNotifications } from 'react-notifications-component';
+import Footer from '../../components/Footer/Footer';
+import { Layout } from '../../layout/Layout';
 
 const articles = () => {
-
+    return (
+        <>
+            <ReactNotifications />
+                <h1>Articles</h1>
+            <Footer />
+        </>
+    )
 }
 
 export default articles;
