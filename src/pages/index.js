0cfb4045173f66a0fe9afbcb82364ab33bd45164
daenamcclintock <daena.mcclintock@gmail.com>
2022-12-01 21:57:52 -0500

    ip tracking

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Contact from '../components/Contact/Contact';
 import Me from '../components/Me/Me';
 import Projects from '../components/Projects/Projects';
@@ -11,9 +11,56 @@ import { ReactNotifications, Store } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
 import 'animate.css/animate.min.css';
 import { Popover } from '@typeform/embed-react'
+import axios from 'axios';
+import emailjs from '@emailjs/browser';
 
 const Home = () => {
 	const [open, setOpen] = useState(false)
+	const [ip, setIp] = useState(null)
+	const [visitor, setVisitor] = useState(null)
+	const [ipDetected, setIPDetected] = useState(false)
+
+	const getIp = async () => {
+		const res = await axios.get('https://geolocation-db.com/json/')
+		console.log('IP User Data', res.data);
+		if (res.data.IPv4) {
+			// setIPDetected(true)
+		}
+		return res.data.IPv4
+	}
+
+	const sendEmail = (ip) => {
+		// e.preventDefault();
+
+		const templateParams = {
+			name: "New Portoflio Website Visitor",
+			email: "Not provided",
+			message: {ip},
+			subject: "New IP Address 😈",
+		}
+		
+		emailjs.send('gmail', 'portfolio_website', templateParams, 'SbJwbvEIoRP7VcCav')
+		  .then((result) => {
+			  console.log(result.text);
+		  },
+		  (error) => {
+			  console.log(error.text);
+		  });
+	  };
+	
+	useEffect(() => {
+		const ipInfo = async () => {
+			await getIp()
+				.then((result) => {
+					console.log(result.toString())
+					sendEmail(result.toString())
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+		}
+		ipInfo()
+	})
 
 	const successNotficiation = () => {
 		Store.addNotification({
@@ -48,21 +95,30 @@ const Home = () => {
 
   return (
 	<>
-		<ReactNotifications />
-		<Layout open={open} setOpen={setOpen}>
-			<BackgroundImage open={open}>
-				<Section open={open}>
-					<Me open={open}/>
-				</Section>
-			</BackgroundImage>
-			<Projects open={open}/>
-			<Technologies open={open}/>
-			<Timeline open={open}/>
-			<Contact successNotficiation={successNotficiation} failNotification={failNotification} open={open}/>
-			<Popover id="o2iHrdTU" />
-		</Layout>
+		{ipDetected ?
+		<div>
+			<h1>Your IP address is: {ip}</h1>
+			<h2>Your location is: {visitor.city}, {visitor.state} {visitor.country_name} {visitor.postal}</h2>
+		</div>
+		:
+		<>
+			<ReactNotifications />
+			<Layout open={open} setOpen={setOpen}>
+				<BackgroundImage open={open}>
+					<Section open={open}>
+						<Me open={open} />
+					</Section>
+				</BackgroundImage>
+				<Projects open={open}/>
+				<Technologies open={open}/>
+				<Timeline open={open}/>
+				<Contact successNotficiation={successNotficiation} failNotification={failNotification} open={open}/>
+				<Popover id="o2iHrdTU" />
+			</Layout>
+		</>
+		}
 	</>
   );
 };
 
-export default Home;
+export default Home
